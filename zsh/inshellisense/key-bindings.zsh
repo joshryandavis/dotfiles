1098739b2b8a562559e58c327fa7ab9a207abfe6
Joshua Davis <114495200+joshryandavis@users.noreply.github.com>
2023-12-14 21:31:21 +0000

    update dotfiles

---
+++ zsh/inshellisense/key-bindings.zsh
@@ -0,0 +1,11 @@
+__inshellisense__() {
+    input=$LBUFFER
+    LBUFFER=
+    inshellisense -c "$input" -s zsh < $TTY
+    print -s $(inshellisense --history)
+    zle reset-prompt
+}
+
+zle     -N   __inshellisense __inshellisense__
+#bindkey '^A' __inshellisense
+bindkey '^@' __inshellisense
