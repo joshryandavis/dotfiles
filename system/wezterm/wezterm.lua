2e749190c6944fe1c51ba8b9c842dd2bdee028eb
Joshua Davis <114495200+joshryandavis@users.noreply.github.com>
2023-12-14 20:36:39 +0000

    update dotfiles

--- system/wezterm/wezterm.lua
+++ system/wezterm/wezterm.lua
@@ -10,20 +10,41 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
--- config.color_scheme = "AdventureTime"
-
 config.window_padding = {
   left = 2,
   right = 2,
-  top = 0,
+  top = 2,
   bottom = 0,
 }
-config.color_scheme = "Catppuccin Mocha"
--- config.use_fancy_tab_bar = false
-config.window_decorations = "RESIZE"
 
+config.font = wezterm.font({
+  family = "JetBrains Mono",
+  -- harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+})
+
+config.font_size = 16.0
+
+config.window_close_confirmation = "NeverPrompt"
+config.keys = {
+  {
+    key = "w",
+    mods = "CMD",
+    action = wezterm.action.CloseCurrentPane({ confirm = false }),
+  },
+}
+
+local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+
+custom.background = "#000000"
+custom.tab_bar.background = "#040404"
+custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
+custom.tab_bar.new_tab.bg_color = "#080808"
+
+config.color_schemes = {
+  ["OLEDppuccin"] = custom,
+}
+config.color_scheme = "OLEDppuccin"
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 -- and finally, return the configuration to wezterm
 return config
